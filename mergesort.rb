30ef6680843d2c6e71107e4b447e16710651f4ba
rock <yazin@whitepayments.com>
2017-03-26 21:29:49 +0100

    Works on any length

--- mergesort.rb
+++ mergesort.rb
@@ -1,7 +1,4 @@
-# Elementary version of mergesort, with following limitations:
-# - input array must have 2^n elements (so we have an even split down to 2 elements)
-#
-# mergesort is *awesome* because it is linear O(nlogn)
+# Ruby implementation of mergesort. mergesort is *awesome* because it is linear O(nlogn)
 
 def mergesort(a)
   # base case
@@ -13,16 +10,14 @@ def mergesort(a)
 end
 
 def merge(a, b)
-  half_index = a.length - 1
-  full_index = a.length + b.length - 1
-
   j = k = 0
   c = []
+  index = a.length + b.length - 1
 
-  for i in 0..full_index
+  for i in 0..index
     # finished with any of the halves?
-    return c += b[k..-1] if j > half_index 
-    return c += a[j..-1] if k > half_index
+    return c += b[k..-1] if j > a.length - 1 
+    return c += a[j..-1] if k > b.length - 1
 
     # compare as normal
     if a[j] <= b[k]
@@ -35,4 +30,4 @@ end
 
 # try it out
 puts mergesort([ 4,  0,  9, 5, 2,  1, 3,  8, 
-                -1, 10, -5, 3, 1, 99, 0, -1, -9]).inspect
+                -1, 10, -5, 3, 6, 99, 0, -2, -9]).inspect
