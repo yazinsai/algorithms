0951c95223507b1f2922ca8c5b015e7b719f2f3e
rock <yazin@whitepayments.com>
2017-04-13 14:30:34 +0100

    Add comments

--- search-rotated-array.rb
+++ search-rotated-array.rb
@@ -1,3 +1,9 @@
+# You're given a sorted array that has been rotated. Write a method
+# that searches for a particular number. Optimize for performance
+
+# This solution uses a variant of binary search, which operates in 
+# O(n) = log(n) time. It's as good as it gets.
+# P.S: Watch out for edge cases! They're a real mind-bender
 def search(a, x) # search for x in a[]
   puts "Called search(#{a.inspect}, #{x})"
   return false if a.length == 0
@@ -6,7 +12,7 @@ def search(a, x) # search for x in a[]
   left, right = 0, -1
   if a[mid] == x
     true
-  elsif a[mid] <= a[right] # sorted
+  elsif a[mid] <= a[right] # sorted, between mid -> right
     if x > a[mid] && x <= a[right]
       search(a.right, x)
     else
@@ -22,10 +28,10 @@ def search(a, x) # search for x in a[]
 end
 
 class Array
-  def left
+  def left # left half of array, excl. mid
     self[0...(self.length/2)]
   end
-  def right
+  def right # right half of array, excl. mid
     self[(self.length/2 + 1)..-1]
   end
 end
