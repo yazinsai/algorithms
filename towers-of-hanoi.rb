3feabe8296eff812cd051651927afea9a471107b
rock <yazin@whitepayments.com>
2017-04-19 09:32:22 +0100

    Optimize

--- towers-of-hanoi.rb
+++ towers-of-hanoi.rb
@@ -2,7 +2,7 @@
 # http://www.mathcs.emory.edu/~cheung/Courses/170/Syllabus/13/hanoi.html
 
 Move = 
-  Struct.new :disk, :from, :to do
+  Struct.new(:disk, :from, :to) do
     def to_s
       "Disk #{disk}: #{from} -> #{to}"
     end
@@ -11,18 +11,19 @@ Move =
 def spare_peg(from, to)
   # returns the peg that is not 'from' nor 'to'
   # e.g. if from="A", to="C" ... then spare="B"
-  [*"A".."C"].each {|e| return e unless [from, to].include? e}
+  ("A".."C").detect{ |peg| ![from, to].include?(peg) }
 end
 
-def hanoi(num, from, to)
-  if num == 1 # base case
-    return [Move.new(num, from, to)]
+def hanoi(num_disks, from, to)
+  if num_disks == 1 # base case
+    return [Move.new(num_disks, from, to)]
   end
   
   spare = spare_peg(from, to)
-  moves = hanoi(num - 1, from, spare) # move everything to the spare peg
-  moves << Move.new(num, from, to) # move the sole remaining disk to the 'to' peg
-  moves += hanoi(num - 1, spare, to) # move all the disks on top of the 'to' peg
+  moves = hanoi(num_disks - 1, from, spare) # move everything to the spare peg
+  moves << Move.new(num_disks, from, to) # move the sole remaining disk to the 'to' peg
+  moves += hanoi(num_disks - 1, spare, to) # move disks back on top of the 'to' peg
 end
 
+# Example run with 3 disks to be moved from A to B
 puts hanoi(3, "A", "B").each {|m| m.to_s}
