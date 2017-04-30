e5e52088293090365466722ee467507d83182bff
rock <yazin@whitepayments.com>
2017-04-30 19:30:31 +0100

    Fixed bug

--- merge-ranges.rb
+++ merge-ranges.rb
@@ -19,11 +19,18 @@ def merge(arr)
       # gap between last slot and this one; can't merge
       merged << arr[i]
     else
-      merged[-1][1] = arr[i][1]
+      # update range-end, if it's further out
+      new_end = arr[i][1]
+      merged[-1][1] = new_end if new_end > merged[-1][1]
     end
   end
   
   merged
 end
 
+def merge_insert(arr, add)
+	merge(arr << add)
+end
+
 puts merge([[1,3],[2,6],[8,10],[15,18]]).inspect
+puts merge_insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,9]).inspect
