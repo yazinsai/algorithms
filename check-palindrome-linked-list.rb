72d5f4e4fe91e58fad7b5c0ba12a92962bd37b7e
rock <yazin@whitepayments.com>
2017-05-08 11:22:42 +0100

    New

--- check-palindrome-linked-list.rb
+++ check-palindrome-linked-list.rb
@@ -1,52 +1,62 @@
 =begin
-Given a singly linked list, determine if it is a palindrome. Could you do it in O(n) time and O(1) space?
+Given a singly linked list, determine if it is a palindrome. Could you 
+do it in O(n) time and O(1) space?
 =end
 
 class Node
-  attr_accessor :data, :next_node
+  attr_reader :val
+  attr_accessor :next
   
-  def initialize(data=nil, next_node=nil)
-    @data = data
-    @next_node = next_node
+  def initialize(val, next_node = nil)
+    @val = val
+    @next = next_node
   end
   
-  def print
-    puts @data
-    @next_node.print if @next_node
+  def to_s
+    "#{@val}-> #{@next.to_s if @next}"
   end
 end
 
-def palindrome?(node)
-  # We're going to use two pointers: slow and fast. Slow pointer moves 1 step
-  # at a time. Fast pointer moves 2 at a time. When fast hits the end of the
-  # list, the slow pointer would be at the middle.
-  # We'll compare the second half of the list with the first half to determine
-  # if the string is a palindrome
-  #   madam   <-- ma,d,am
-  #   racecar <-- rac,e,car
-  slow = fast = node
+def palindrome?(head)
+  slow, fast = head, head
   
-  while !fast.nil? do
+  # fast forward to end of list (n/2 time)
+  while fast && fast.next do
     slow = slow.next
-    fast = slow.next if slow
+    fast = fast.next.next
   end
   
-  # Now, slow points to middle element
-  start = node
-  while slow.next do
-    return false unless start.data == slow.data
-  end
-  # Error: This previous loop compares the left half of the palindrome
-  # e.g. "ma" from madam with the right half "am" in reverse order. We could
-  # either use a stack to push/pop the chars to verify they're the same OR
-  # we'll have to reverse one of the sides
+  # reverse second half of list (n/2 time)
+  reversed = reverse(slow)
   
+  # compare the two halves (n/2 time)
+  while reversed do
+    return false if reversed.val != head.val
+    reversed = reversed.next
+    head = head.next
+  end
   true
 end
 
-n = Node.new("A",
-      Node.new("B",
+def reverse(head)
+  # in place reversal of a linked list
+  prev = nil
+  current = head
+  
+  while current do
+    temp = current.next
+    current.next = prev
+    
+    prev = current
+    current = temp
+  end
+  prev
+end
+
+node =
+  Node.new("A",
+    Node.new("C",
+      Node.new("C",
         Node.new("C",
-          Node.new("A") )))
-n.print
-puts palindrome?(n).inspect
+          Node.new("A")))))
+puts palindrome?(node).inspect
