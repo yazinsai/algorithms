1aa065d60bee8211297e4f3418a02bd4d7b6f630
rock <yazin@whitepayments.com>
2017-05-09 14:07:08 +0100

    Improved

--- largest-decent.rb
+++ largest-decent.rb
@@ -1,18 +1,12 @@
 # Source: https://www.hackerrank.com/challenges/sherlock-and-the-beast
 
 def largest_decent(n)
-  # find decent numbers, in decreasing value
-  return ("5" * n).to_i if n % 3 == 0
-  
   for i in (n/3).downto(0)
-    n5 = i * 3 # number of 5's
-    n3 = n - n5 # remainder
-    return ("5" * n5 + "3" * n3).to_i if n3 > 0 && n3 % 5 == 0
+    n5 = i * 3
+    n3 = n - n5
+    return "5" * n5 + "3" * n3 if n3 % 5 == 0
   end
-  
-  return ("3" * n).to_i if n % 5 == 0
-  
-  -1
+	-1
 end
 
 puts largest_decent(11)
